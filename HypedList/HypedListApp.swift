dcfb939359b2770d70e8e48a87cc25fa99c7b9fd
hanmok <dmammmm@naver.com>
2021-08-28 23:44:13 +0900

    Initial Commit

---
+++ HypedList/HypedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypedListApp.swift
+//  HypedList
+//
+//  Created by Mac mini on 2021/08/28.
+//
+
+import SwiftUI
+
+@main
+struct HypedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
